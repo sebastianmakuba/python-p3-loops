72d5066d23f4f2b418506368d7f42c0c90bdf8d3
+ <+>
2023-08-16 14:55:32 +0300

    Completed tasks

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,38 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+  
+#     # count = 10
+#     # while count >= 1:
+#     #     print(count)
+#     #     count -= 1
+    for i in range(11):
+        print(i)
+    print("Happy New Year!")
     pass
+happy_new_year()
 
 def square_integers(int_list):
     # code goes here!
-    pass
+   squares = [i**2 for i in int_list]
+   return (squares)
+   print (squares)
+   pass
+square_integers([1,2,3,4,5])
+    
+
 
 def fizzbuzz():
     # code goes here!
+    for i in range(1, 101) : 
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0 :
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+      
+        else :
+            print(i)
     pass
+fizzbuzz()
